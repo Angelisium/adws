2a9bd2afefbd948c3ac2460e00576eccacea143a
Scrooge McDuck <angelisium@daum.net>
2022-11-07 12:22:08 +0100

    add typescript sample

---
+++ websites/samplets/source/main.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+
+app.use(express.static('public'));
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+	res.send("Root route is working");
+});
+
+app.listen(3000, () => {
+	console.log("Server listening on port 3000");
+});
